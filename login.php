1c43e19d0cc9b7dbcc917d6b396a332b5d49c07e
manulydia <98188241+manulydia@users.noreply.github.com>
2022-12-08 22:07:44 -0800

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+
+include 'config.php';
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password_user']));
+
+   $select = mysqli_query($conn, "SELECT * FROM customers WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $row = mysqli_fetch_assoc($select);
+      $_SESSION['user_id'] = $row['id'];
+      header('location:index.php');
+   }else{
+      $message[] = 'incorrect password or email!';
+   }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +35,16 @@
 
 </head>
 <body>
-   
+
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
+   }
+}
+?>
+
+
 <div class="form-container">
 
    <form action="" method="post">
@@ -20,7 +53,7 @@
       <input type="password" name="password" required placeholder="enter password" class="box">
       <input type="submit" name="submit" class="btn" value="login now">
       <p>don't have an account? <a href="register.php">register now</a></p>
-      <p>continue as a guest? <a href="">start shopping</a></p>
+      <p>want to continue as a guest? <a href="">start shopping</a></p>
    </form>
 
 </div>
